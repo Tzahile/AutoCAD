f6497188275ef420a0dda60ea894ae27cf2aec77
Dor-kpf <33734978+Dor-kpf@users.noreply.github.com>
2017-12-27 15:38:07 +0200

    added README file. removed center_point in Square class.

--- Square.java
+++ Square.java
@@ -1,15 +1,14 @@
 public class Square extends Rectangle
 {
-	private Point center_point;
 	public Square(Color color, double x1, double y1, double length) 
 	{
 		super(color, x1 - length / 2, y1 + length / 2, x1 + length / 2, y1 - length / 2);
-		center_point = new Point(x1, y1);
 		CalculateRemainingVertex();
 	}
 	
 	public Square Clone() {
-		Square clone = new Square(color, center_point.getX(), center_point.getY(), first_edge_length);
+		Square clone = new Square(color, vertex[0].getX() + first_edge_length/2,
+				vertex[0].getY() - first_edge_length / 2, first_edge_length);
 		return clone;
 	}
 }
